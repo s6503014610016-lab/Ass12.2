f40812b59daf94684a7e324884d37f0ea69edfc1
s6503014610016 <s6503014610016@kmutnb.ca.th>
2025-10-02 03:59:27 +0700

    First ESP32 Blink project with PlatformIO

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@ void setup() {
 void loop() {
     digitalWrite(LED_PIN, HIGH);
     Serial.println("LED ON");
-    delay(2000);
+    delay(500);
 
     digitalWrite(LED_PIN, LOW);
     Serial.println("LED OFF");
-    delay(2000);
+    delay(500);
 }
